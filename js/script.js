ac64cd50b47088691e4989ffa7f21dd397af51fe
Garden-of-Eden-pattern <dyuia0303@icloud.com>
2022-03-14 08:52:10 +0900

    add hover event

--- js/script.js
+++ js/script.js
@@ -2,4 +2,50 @@ $(function(){
 
     $('#footer-image').fadeOut(2000);
 
+    $('#math1a').hover(
+
+        function(){
+
+            $('#text1').fadeIn();
+
+        },
+
+        function(){
+
+            $('#text1').fadeOut();
+
+        });
+    
+    $('#math2b').hover(
+
+        function(){
+
+            $('#text2').fadeIn();
+
+        },
+
+        function(){
+
+            $('#text2').fadeOut();
+
+        }
+    );
+
+    $('#math3').hover(
+
+        function(){
+
+            $('#text3').fadeIn();
+
+        },
+
+        function(){
+
+            $('#text3').fadeOut();
+
+        }
+
+    );
+
+
 })
